21fc639f6e3c41decf122476eedd3e58b17513fb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 04:33:40 +0900

    HTTP通信を行ってHello, Worldと表示させるようにした。

--- Backend/main.go
+++ Backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, World")
+}
 
 func main() {
-	fmt.Println("Hello, World.")
+	fmt.Println("Start Auth Server...")
+	http.HandleFunc("/", index)
+	err := http.ListenAndServe(":3000", nil)
+    if err != nil {
+		fmt.Println("Failed...")
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
