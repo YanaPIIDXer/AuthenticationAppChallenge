9fd5d5905cf3c6402906d4a565a2e5175e867296
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 05:22:40 +0900

    とりあえずJSONを返してみた。

--- Backend/main.go
+++ Backend/main.go
@@ -4,10 +4,29 @@ import(
 	"fmt"
 	"net/http"
 	"log"
+	"encoding/json"
 )
 
+// アカウント
+// TODO:別の所に移す。
+type Account struct {
+	Id int `json:"id"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World")
+	var account Account
+	account.Id = 1
+	account.Email = "hoge@hoge"
+	account.Password = "PASSWORD"
+
+    j1, err := json.Marshal(account)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	fmt.Fprintln(w, string(j1))
 }
 
 func main() {
