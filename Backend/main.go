4e7714b13891a9836930650c1d47016328f5481c
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 08:55:26 +0900

    サーバのメインプログラムを別パッケージに移動。

--- Backend/main.go
+++ Backend/main.go
@@ -1,34 +1,10 @@
 package main
 
-import(
-	"fmt"
-	"net/http"
-	"log"
-	"encoding/json"
-
-	"auth"
+import (
+	"server"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	var account auth.Account
-	account.Id = 1
-	account.Email = "hoge@hoge"
-	account.Password = "PASSWORD"
-
-    j1, err := json.Marshal(account)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-	fmt.Fprintln(w, string(j1))
-}
 
 func main() {
-	fmt.Println("Start Auth Server...")
-	http.HandleFunc("/", index)
-	err := http.ListenAndServe(":3000", nil)
-    if err != nil {
-		fmt.Println("Failed...")
-        log.Fatal("ListenAndServe: ", err)
-    }
+	server.Start()
 }
---
+++ Backend/server/authserver.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"encoding/json"
+
+	"auth"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	var account auth.Account
+	account.Id = 1
+	account.Email = "hoge@hoge"
+	account.Password = "PASSWORD"
+
+    j1, err := json.Marshal(account)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+	fmt.Fprintln(w, string(j1))
+}
+
+func Start() {
+	fmt.Println("Start Auth Server...")
+	http.HandleFunc("/", index)
+	err := http.ListenAndServe(":3000", nil)
+    if err != nil {
+		fmt.Println("Failed...")
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
