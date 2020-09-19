a0d85f01299d6c435ef11fcdafb693d91875b730
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 12:06:17 +0900

    ボタン押下時に走らせるメソッドを定義。 ※中身はとりあえずダミーでダイアログを出すようにしている。

--- Frontend/Src/components/LoginForm.vue
+++ Frontend/Src/components/LoginForm.vue
@@ -5,14 +5,20 @@
       <br />
       <input type="password" name="password" placeholder="Input your password." />
       <br />
-      <input type="button" value="Sign in" />
+      <input type="button" v-on:click="login()" value="Login" />
       <br />
     </form>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    login: function (event) {
+      alert("Login");
+    },
+  },
+};
 </script>
 
 <style>
