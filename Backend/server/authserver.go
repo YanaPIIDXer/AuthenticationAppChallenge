f06ded0b850d3c19f65a4486fec76406c6b78a50
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 09:02:39 +0900

    ログイン用のAPIの実装準備。

--- Backend/server/authserver.go
+++ Backend/server/authserver.go
@@ -4,28 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"log"
-	"encoding/json"
-
-	"auth"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
-	var account auth.Account
-	account.Id = 1
-	account.Email = "hoge@hoge"
-	account.Password = "PASSWORD"
-
-    j1, err := json.Marshal(account)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-	fmt.Fprintln(w, string(j1))
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, r.Body)
 }
 
 func Start() {
 	fmt.Println("Start Auth Server...")
-	http.HandleFunc("/", index)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":3000", nil)
     if err != nil {
 		fmt.Println("Failed...")
