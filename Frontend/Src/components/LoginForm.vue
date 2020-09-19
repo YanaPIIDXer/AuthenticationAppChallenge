fc7b5f627862963d1be40f627cc38b873874cb1b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 14:25:54 +0900

    フォームに入力された値を投げるようにした。

--- Frontend/Src/components/LoginForm.vue
+++ Frontend/Src/components/LoginForm.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <form method="POST">
-      <input type="text" name="email" placeholder="Input your email address." />
+      <input type="text" v-model="email" placeholder="Input your email address." />
       <br />
-      <input type="password" name="password" placeholder="Input your password." />
+      <input type="password" v-model="password" placeholder="Input your password." />
       <br />
       <input type="button" v-on:click="login()" value="Login" />
       <br />
@@ -19,8 +19,8 @@ export default {
     async login() {
       const res = await axios
         .post("http://yanap.docker.com:3000/login", {
-          email: "hoge",
-          password: "fuga",
+          email: this.email,
+          password: this.password,
         })
         .catch((err) => alert(err));
       if (!res || !res.data) {
