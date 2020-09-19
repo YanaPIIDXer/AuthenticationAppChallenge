3a0124a87e43ed90b9e0560906129cdff2fe2344
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 11:55:48 +0900

    ログインフォーム作成。

---
+++ Frontend/Src/components/LoginForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <form>
+      <input type="text" name="email" placeholder="Input your email address." />
+      <br />
+      <input type="password" name="password" placeholder="Input your password." />
+      <br />
+      <input type="submit" value="Sign in" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- Frontend/Src/pages/index.vue
+++ Frontend/Src/pages/index.vue
@@ -1,32 +1,17 @@
 <template>
   <div class="container">
-    <div>
-      <h1 class="title">auth</h1>
-    </div>
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {};
+import LoginForm from "~/components/LoginForm.vue";
+export default {
+  components: {
+    LoginForm,
+  },
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 </style>
