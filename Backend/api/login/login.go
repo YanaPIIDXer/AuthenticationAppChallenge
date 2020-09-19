92ab823f0abcf82b0f9ba13124577d9ace20b526
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-19 09:33:55 +0900

    リクエストをJSONにパースするようにした。

--- Backend/api/login/login.go
+++ Backend/api/login/login.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"encoding/json"
+	"strconv"
 
 	"api/login/result_code"
 )
@@ -17,10 +18,31 @@ type LoginRequest struct {
 // ログイン結果
 type LoginResult struct {
 	ResultCode int `json:"result_code"`
+	ErrorMessage string `json:"error_message"`
 }
 
 // API実行
 func API(w http.ResponseWriter, r *http.Request) {
+	length, err := strconv.Atoi(r.Header.Get("Content-Length"))
+	body := make([]byte, length)
+	length, err = r.Body.Read(body)
+
+	var request LoginRequest
+	err = json.Unmarshal(body, &request)
+	if err != nil {
+		var result LoginResult
+		result.ResultCode = result_code.Fatal
+		result.ErrorMessage = err.Error()
+		
+		j1, err := json.Marshal(result)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Fprintln(w, string(j1))
+		return
+	}
+	
 	var result LoginResult
 	result.ResultCode = result_code.LoginSuccess
     j1, err := json.Marshal(result)
