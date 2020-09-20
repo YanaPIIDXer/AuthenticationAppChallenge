38abc6b38bf0436d6a554833097883275695fd78
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2020-09-21 07:46:23 +0900

    ユーザ登録処理が正常に動くようにした。

--- Backend/auth/account.go
+++ Backend/auth/account.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"database/sql"
+	"fmt"
 
 	"api/register/result_code"
 	"db"
@@ -12,14 +13,43 @@ func RegisterBasicAuth(email string, password string) int {
 	if email == "" || password == "" { return result_code.EmptyParam }
 
 	var resultCode = result_code.RegisterSuccess
-	msqldrv.Access(func(db *sql.DB) {		
-		_, err := db.Query("SELECT id FROM basic_auth where email=?", email)
+	err := msqldrv.Access(func(db *sql.DB) {		
+		var dummy = 0
+		err := db.QueryRow("SELECT id FROM basic_auth where email=?", email).Scan(&dummy)
 		if err == nil {
 			resultCode = result_code.UsedEmail
+			return
 		} else if err != sql.ErrNoRows {
+			fmt.Println(err.Error())
 			resultCode = result_code.Fatal
+			return
 		}
+
+		tx, _ := db.Begin()
+		stmt, err := db.Prepare("INSERT INTO accounts VALUES()")
+		result, err := stmt.Exec()
+		if err != nil {
+			fmt.Println(err.Error())
+			resultCode = result_code.Fatal
+			tx.Rollback()
+			return
+		}
+
+		id, _ := result.LastInsertId()
+		err = db.QueryRow("INSERT INTO basic_auth VALUES(?, ?, ?)", id, email, password).Scan()
+		if err != sql.ErrNoRows {
+			fmt.Println(err.Error())
+			resultCode = result_code.Fatal
+			tx.Rollback()
+			return
+		}
+		tx.Commit()
 	})
+
+	if err != nil {
+		resultCode = result_code.Fatal
+		fmt.Println(err.Error())
+	}
 	
 	return resultCode
 }
--- Backend/db/driver.go
+++ Backend/db/driver.go
@@ -6,12 +6,12 @@ import (
 )
 
 // DBアクセス
-func Access(callback func(db *sql.DB)) (bool, error) {
-	db, err := sql.Open("mysql", "root:root/auth_app")
-	if err != nil { return false, err }
+func Access(callback func(db *sql.DB)) error {
+	db, err := sql.Open("mysql", "root:root@tcp(db)/auth_app")
+	if err != nil { return err }
 	defer db.Close()
 
 	callback(db)
 
-	return true, nil
+	return nil
 }
